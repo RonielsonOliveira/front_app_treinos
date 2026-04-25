22d893920640c6f958e84f95711449792e274d43
vboxuser <ronielsonoliveira@live.com>
2026-04-25 09:29:31 -0300

    railway new server

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://apitreinos-production.up.railway.app",
+  baseURL: "https://apitreinos-production-85fe.up.railway.app",
 });
