01a004462152587d5444168bb61f351fdfe657ae
vboxuser <ronielsonoliveira@live.com>
2026-07-10 23:54:47 -0300

    photo preview  adjustments

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -32,11 +32,19 @@ export default createGlobalStyle`
     height: 100%;
   }
 
-button {
-  cursor: pointer;
-  font-family: inherit;
-}
-
+  button {
+    cursor: pointer;
+    background: ${primaryColor};
+    border: none;
+    color: #fff;
+    padding:  10px 20px;
+    border-radius: 4px;
+    font-weight: 700;
+    transition: all 300ms;
+  }
+  button:hover{
+    filter: brightness(75%);
+  }
 
   a {
     text-decoration: none;
